fb5d103c0e3d60d1de442f7610e6155342b0a6fc
joao mateus <jaomateus@gmail.com>
2024-08-13 09:41:42 +0200

    add adress to resztaurants

---
+++ db/migrate/20240813073032_add_adress_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddAdressToRestaurants < ActiveRecord::Migration[7.2]
+  def change
+    add_column :restaurants, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_13_072546) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_13_073032) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 end
